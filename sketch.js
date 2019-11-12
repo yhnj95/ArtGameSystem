510f19f5ef9dc5c4e2112cebe045ad955890aed4
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-11-12 11:11:56 +0900

    test-1

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
-let canvas;
-let canvasWidth = 600;
-let canvasHeight = 400;
+
 
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+createCanvas(800,400);
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(mouseX, mouseY, 150, 150);
+  background(255,255,255);
+  textAlign(CENTER);
+  text('마우스를 눌러봐!');
+  drawSprites();
+}
+
+function mousePressed() {
+  var s = createSprite(mouseX, mouseY, 15, 15);
+  s.velocity.x = random(-5, 5)
+  s.velocity.y = random(-5, 5)
 }
