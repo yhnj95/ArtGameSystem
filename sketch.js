5d9c73429d8fd72e3c88b43aed4554b00f71dbc8
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-11-19 11:32:54 +0900

    normal_game

--- sketch.js
+++ sketch.js
@@ -1,15 +1,67 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
+let frog;
+let car1;
+let goal;
+let sound_hit;
+
+function preload() {
+  sound_hit = loadSound('hit.wav');
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
   noCursor();
+  resetGame();
 }
 
 function draw() {
-  background(255);
-  fill(0);
-  rect(mouseX, mouseY, 150, 150);
+  background(220);
+
+if(car1.position.x >=width) {
+  car1.position.x = 0;
+  ca1.setVelocity(random(3, 10), 0);
+}
+if (frog.bounce(car1)){
+  sound_hit.play();
+}
+if (frog.overlap(goal)){
+  nextLevel();
+}
+drawSprites();
+checkGameOver();
+}
+
+function resetGame() {
+  frog = createSprite(width/2, height-30, 20, 40);
+  goal = createSprite(width/2, 0, width, 4);
+  car1 = createSprite(0, height/2, 60, 30);
+  car1.setVelocity(random(3, 10), 0);
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    frog.position.y -= 10;
+  }
+}
+function checkGameOver() {
+  if (frog.position.x <= 0 || width <= frog.position.x) {
+    fill(255, 0, 0);
+    textSize(60);
+    textAlign(CENTER);
+    text("GAME OVER", width/2, height/2);
+
+  }
+}
+
+
+function nextLevel() {
+  frog.position.x = width/2;
+  frog.position.y = height-30;
+}
+
+
+function playHitSound() {
+  sound_hit.play();
 }
