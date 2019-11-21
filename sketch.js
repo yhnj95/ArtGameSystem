f08f2420d531a5265ce58b98aa1c603df3557930
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-11-21 22:29:40 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
+let canvas;
+let canvasWidth = 600;
+let canvasHeight = 400;
 let frog;
 let car1;
 let goal;
 
 function preload() {
+  sound_hit = loadSound('hit.wav');
 }
 
 function setup() {
-  createCanvas(400, 400);
-
+  createCanvas(canvasWidth, canvasHeight);
+  canvas.position(windowWidth/2 - canvasWidth/2, 20);
   resetGame();
+  noCursor();
 
 }
 
