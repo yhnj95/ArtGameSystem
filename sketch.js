f0dac9c3c07b76e509c0d62a8a1140f8ef917aca
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-12-06 20:36:52 +0900

    도형 만들기

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
-var heart;
+var rock;
+
+function preload() {
+  rock = loadAnimation('lib/heart.png');
+}
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
+  rock = createSprite (350, 200);
+  rock.mouseActive = true;
 }
 
 function draw() {
   background(255);
   fill(0);
   rect(0, 0, 300, 400);
-  heart = ellipse(350, 200, 45, 45);
-
- heart.onMousePressed = function() {
-   heart.position.x = mouseX;
-   heart.position.y = mouseY;
- }
+  drawSprites();
 }
