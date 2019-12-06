e102d3f52b5b0c50f8e80e71b3f316760515d375
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-12-06 22:09:04 +0900

    다르게 도형을 만져보자

--- sketch.js
+++ sketch.js
@@ -1,41 +1,64 @@
+
+let bx;
+let by;
+let boxSize = 75;
+let overBox = false;
+let locked = false;
+let xOffset = 0.0;
+let yOffset = 0.0;
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
-var rock;
-
-function preload() {
-  rock = createSprite('lib/.png');
-}
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  rock = createSprite(350, 200);
-  rock.addAnimation('normal', 'lib/R1.png', 'lib/R3.png');
-  rock.addAnimation('stretch', 'lib/R1.png', 'lib/R8.png');
-  var anim = rock.addAnimation('transform', 'lib/R1.png', 'lib/R8.png');
 
-  rock.setCollider('circle', 0, 0, 64);
-
-  rock.onMouseOver = function() {
-    this.changeAnimation('stretch');
-  };
-  rock.onMouseOut = function() {
-    this.changeAnimation('normal');
-  };
-  rock.onMousePressed = function() {
-    this.changeAnimation('transform');
-    this.animation.goToFrame(this.animation.getLastFrame());
-  };
-  rock.onMouseReleased = function() {
-    this.changeAnimation('transform');
-    this.animation.goToFrame(0);
-  };
+  bx = 400;
+  by = 300;
+  rectMode(RADIUS);
+  strokeWeight(0);
 }
 
 function draw() {
   background(255);
   fill(0);
   rect(0, 0, 300, 400);
-  drawSprites();
+  if (
+    mouseX > bx - boxSize &&
+    mouseX < bx + boxSize &&
+    mouseY > by - boxSize &&
+    mouseY < by + boxSize
+  ) {
+    overBox = true;
+    if (!locked) {
+      fill(244, 122, 158);
+    }
+  } else {
+    fill(0);
+    overBox = false;
+  }
+  ellipse(bx, by, boxSize);
+}
+
+function mousePressed() {
+  if (overBox) {
+    locked = true;
+    fill(255, 255, 255);
+  } else {
+    locked = false;
+  }
+  xOffset = mouseX - bx;
+  yOffset = mouseY - by;
+}
+
+function mouseDragged() {
+  if (locked) {
+    bx = mouseX - xOffset;
+    by = mouseY - yOffset;
+  }
+}
+
+function mouseReleased() {
+  locked = false;
 }
