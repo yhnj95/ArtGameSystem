11743f8d06e2b7d945a505c90de4cc94e0a2c70c
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-12-06 21:55:58 +0900

    애니메이션을 준 도형

--- sketch.js
+++ sketch.js
@@ -4,14 +4,33 @@ let canvasHeight = 400;
 var rock;
 
 function preload() {
-  rock = loadAnimation('lib/heart.png');
+  rock = createSprite('lib/.png');
 }
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  rock = createSprite (350, 200);
-  rock.mouseActive = true;
+  rock = createSprite(350, 200);
+  rock.addAnimation('normal', 'lib/R1.png', 'lib/R3.png');
+  rock.addAnimation('stretch', 'lib/R1.png', 'lib/R8.png');
+  var anim = rock.addAnimation('transform', 'lib/R1.png', 'lib/R8.png');
+
+  rock.setCollider('circle', 0, 0, 64);
+
+  rock.onMouseOver = function() {
+    this.changeAnimation('stretch');
+  };
+  rock.onMouseOut = function() {
+    this.changeAnimation('normal');
+  };
+  rock.onMousePressed = function() {
+    this.changeAnimation('transform');
+    this.animation.goToFrame(this.animation.getLastFrame());
+  };
+  rock.onMouseReleased = function() {
+    this.changeAnimation('transform');
+    this.animation.goToFrame(0);
+  };
 }
 
 function draw() {
