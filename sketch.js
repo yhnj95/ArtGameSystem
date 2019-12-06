a274c4f68e0fec13c8c921eaa43fd30f8f9402ce
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-12-06 19:53:27 +0900

    원 만들어보기

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
 
+
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
@@ -11,6 +12,6 @@ function draw() {
   background(255);
   fill(0);
   rect(0, 0, 300, 400);
-
+  ellipse(450, 200, 40, 40);
 
 }
