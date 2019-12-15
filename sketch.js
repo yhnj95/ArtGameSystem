f1db72d84d32074cad250555597e09e7f85c71a5
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-12-15 22:46:17 +0900

    큰일이다.. 머지 실수...

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
 
 let bx;
 let by;
-<<<<<<< HEAD
-let boxSize = 75;
-=======
 let bx1;
 let by1;
 let boxSize = 60;
->>>>>>> New_Forgger
 let overBox = false;
 let locked = false;
 let xOffset = 0.0;
 let yOffset = 0.0;
-<<<<<<< HEAD
-=======
 
->>>>>>> New_Forgger
 let canvas;
 let canvasWidth = 600;
 let canvasHeight = 400;
@@ -24,15 +17,10 @@ function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/2 - canvasWidth/2, 20);
 
-<<<<<<< HEAD
-  bx = 400;
-  by = 300;
-=======
   bx = 450;
   by = 200;
 
 
->>>>>>> New_Forgger
   rectMode(RADIUS);
   strokeWeight(0);
 }
@@ -41,21 +29,6 @@ function draw() {
   background(255);
   fill(0);
   rect(0, 0, 300, 400);
-<<<<<<< HEAD
-  fill(255,255,255);
-  ellipse(50,80,30,30);
-  ellipse(50,160,25,25);
-  ellipse(50,240,10,10);
-  ellipse(50,320,10,10);
-  ellipse(150,80,10,10);
-  ellipse(150,160,25,25);
-  ellipse(150,240,47,47);
-  ellipse(150,320,35,35);
-  ellipse(250,80,30,30);
-  ellipse(250,160,17,17);
-  ellipse(250,240,12,12);
-  ellipse(250,320,48,48);
-=======
   fill(200);
   ellipse(100,120,80,80);
   ellipse(190,120,80,80);
@@ -73,7 +46,6 @@ function draw() {
   ellipse(482.25,232.25,18);
   ellipse(450,245,18);
 
->>>>>>> New_Forgger
   if (
     mouseX > bx - boxSize &&
     mouseX < bx + boxSize &&
@@ -82,17 +54,10 @@ function draw() {
   ) {
     overBox = true;
     if (!locked) {
-<<<<<<< HEAD
-      fill(255);
-    }
-  } else {
-    fill(0);
-=======
       fill(255,123,0);
     }
   } else {
     fill(255,123,0);
->>>>>>> New_Forgger
     overBox = false;
   }
   ellipse(bx, by, boxSize);
