02e15a8bcbdfa397f20dfe4c3a0e9fcddf3a20ed
yhnj95 <57379413+yhnj95@users.noreply.github.com>
2019-12-15 21:48:39 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -142,6 +142,17 @@ function mouseDragged() {
   }
 }
 
+function mouseDragged() {
+  if (locked) {
+    bx2 = mouseX - xOffset;
+    by2 = mouseY - yOffset;
+  }
+  if (locked) {
+    bx3 = mouseX - xOffset;
+    by3 = mouseY - yOffset;
+  }
+}
+
 function mouseReleased() {
   locked = false;
 }
